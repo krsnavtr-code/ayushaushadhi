7683bb9b6466082b502c3b27df1d52d6dc33e8ad
Krishna Avtar <krishnaavtar955@gmail.com>
2025-12-19 11:03:50 +0530

    fixing media upload

--- server/route/uploadRoute.js
+++ server/route/uploadRoute.js
@@ -514,7 +514,7 @@ router.delete('/file/:filename', protect, async (req, res) => {
 });
 
 router.options('*', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', process.env.FRONTEND_URL || 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Origin', '/api');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
