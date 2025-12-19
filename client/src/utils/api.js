0364b15b3e5932975ffd59b4549848d837b499a2
Krishna Avtar <krishnaavtar955@gmail.com>
2025-12-19 11:19:23 +0530

    fix build error and api point

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,7 @@ import { toast } from 'react-hot-toast';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4002/api',
+  baseURL: '/api', 
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
